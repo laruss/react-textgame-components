4c50f8ad1a77453f04d79f01ba33386e51116c19
konstantin <konstantin@wisebits.com>
2023-12-15 23:30:45 +0200

    removed peerDependencies

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <div>
-            TODO
-        </div>
+        <div>TODO</div>
     </React.StrictMode>,
 );
